07e90663a89948d52cde1f2b41289766faf7c92c
Fabien Loup <fabien@norydev.com>
2023-09-29 12:14:27 +0200

    Add some seed data to play with in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.create!(name: "Stay Hydrated", description: "Drink at least 2l of water", due_date: 3.hours.from_now)
+Task.create!(name: "Read all the books", description: "Start with upper shelf")
+Task.create!(name: "Write more tasks")
+
+Note.create!(name: "Books I read", content: ["Treasure Island", "A tale of two cities", "Les misérables"])
+Note.create!(name: "Great German Food")
